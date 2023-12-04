cff680c104ac8163e22f66db33fb21df293ce441
[200~ <hamzaalchamak442979@gmail.com>
2023-12-05 01:20:43 +0300

    just close button fixed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const showMenu = () => {
     document.querySelector('.mobile-menu').classList.remove('mobile-menu');
   };
 const removeMenu = () => {
-    document.querySelector('.mobile-menu').classList.add('mobile-menu');
+    document.querySelector('.show-mobile-menu').classList.add('mobile-menu');
     document.querySelector('.mobile-menu').classList.remove('show-mobile-menu');
 };
 icon.addEventListener('click',showMenu);
