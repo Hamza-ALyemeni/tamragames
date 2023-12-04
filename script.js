2396621143e26da9a90c7e6a5150a59a2e48a1f9
[200~ <hamzaalchamak442979@gmail.com>
2023-12-05 01:05:55 +0300

    make navbar responsive

---
+++ script.js
@@ -0,0 +1,12 @@
+const icon = document.querySelector('.icon');
+const clos = document.querySelector('.closebtn');
+const showMenu = () => {
+    document.querySelector('.mobile-menu').classList.add('show-mobile-menu');
+    document.querySelector('.mobile-menu').classList.remove('mobile-menu');
+  };
+const removeMenu = () => {
+    document.querySelector('.mobile-menu').classList.add('mobile-menu');
+    document.querySelector('.mobile-menu').classList.remove('show-mobile-menu');
+};
+icon.addEventListener('click',showMenu);
+clos.addEventListener('click',removeMenu);
